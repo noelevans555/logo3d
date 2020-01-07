3dec7461fadd3fb570cacde1fc3b3be6fe18946f
Noel Evans <noele@amazon.com>
2020-01-06 20:53:36 -0800

    Add the top-level LogoCompiler (process orchestrator)

---
+++ src/main/java/com/noelevans555/logo3d/compiler/LogoCompiler.java
@@ -0,0 +1,56 @@
+package com.noelevans555.logo3d.compiler;
+
+import java.util.List;
+
+import com.noelevans555.logo3d.compiler.exception.CompilerException;
+import com.noelevans555.logo3d.compiler.exception.StopException;
+import com.noelevans555.logo3d.compiler.program.Program;
+import com.noelevans555.logo3d.compiler.program.ProgramFactory;
+import com.noelevans555.logo3d.compiler.program.tokens.TokenReader;
+import com.noelevans555.logo3d.compiler.program.tokens.Tokenizer;
+import com.noelevans555.logo3d.compiler.state.State;
+import com.noelevans555.logo3d.compiler.state.StateFactory;
+import com.noelevans555.logo3d.compiler.turtle.Turtle;
+import com.noelevans555.logo3d.compiler.turtle.TurtleFactory;
+import com.noelevans555.logo3d.model.LogoLine;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * The Logo3d compiler, responsible for translating a Logo3d program into the
+ * lines traversed when the program is run.
+ *
+ * @author Noel Evans (noelevans555@gmail.com)
+ */
+
+@AllArgsConstructor
+public class LogoCompiler {
+
+    private final Tokenizer tokenizer;
+    private final ProgramFactory programFactory;
+    private final StateFactory stateFactory;
+    private final TurtleFactory turtleFactory;
+
+    /**
+     * Compiles and runs the specified Logo3d program, returning the lines traversed
+     * during execution.
+     *
+     * @param program A Logo3d program defined as a single string.
+     * @return The lines traversed when running the Logo3d program.
+     * @throws CompilerException If the program cannot be compiled or run.
+     */
+    public List<LogoLine> compileProgram(final String program) throws CompilerException {
+        TokenReader tokenReader = tokenizer.tokenize(program);
+        Program compiledProgram = programFactory.buildProgram(tokenReader);
+        State state = stateFactory.buildState();
+        Turtle turtle = turtleFactory.buildTurtle();
+        try {
+            compiledProgram.run(state, turtle);
+        } catch (StopException stop) {
+            // Deliberately consumed. Thrown by stop commands encountered in the top-level
+            // program.
+        }
+        return turtle.getLogoLines();
+    }
+
+}
---
+++ src/test/java/com/noelevans555/logo3d/compiler/LogoCompilerTest.java
@@ -0,0 +1,80 @@
+package com.noelevans555.logo3d.compiler;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.google.common.collect.ImmutableList;
+import com.noelevans555.logo3d.compiler.exception.StopException;
+import com.noelevans555.logo3d.compiler.program.Program;
+import com.noelevans555.logo3d.compiler.program.ProgramFactory;
+import com.noelevans555.logo3d.compiler.program.tokens.TokenReader;
+import com.noelevans555.logo3d.compiler.program.tokens.Tokenizer;
+import com.noelevans555.logo3d.compiler.state.State;
+import com.noelevans555.logo3d.compiler.state.StateFactory;
+import com.noelevans555.logo3d.compiler.turtle.Turtle;
+import com.noelevans555.logo3d.compiler.turtle.TurtleFactory;
+import com.noelevans555.logo3d.model.LogoColor;
+import com.noelevans555.logo3d.model.LogoLine;
+import com.noelevans555.logo3d.model.LogoPoint;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LogoCompilerTest {
+
+    private static final String TEST_PROGRAM = "test program";
+    private static final List<LogoLine> TURTLE_OUTPUT = ImmutableList
+            .of(new LogoLine(new LogoPoint(0, 0, 0), new LogoPoint(1, 1, 1), new LogoColor(255, 255, 255)));
+
+    @Mock
+    private TokenReader tokenReader;
+    @Mock
+    private Tokenizer tokenizer;
+    @Mock
+    private Program program;
+    @Mock
+    private ProgramFactory programFactory;
+    @Mock
+    private State state;
+    @Mock
+    private StateFactory stateFactory;
+    @Mock
+    private Turtle turtle;
+    @Mock
+    private TurtleFactory turtleFactory;
+
+    @InjectMocks
+    private LogoCompiler logoCompiler;
+
+    @Before
+    public void setup() throws Exception {
+        when(tokenizer.tokenize(TEST_PROGRAM)).thenReturn(tokenReader);
+        when(programFactory.buildProgram(tokenReader)).thenReturn(program);
+        when(stateFactory.buildState()).thenReturn(state);
+        when(turtleFactory.buildTurtle()).thenReturn(turtle);
+        when(turtle.getLogoLines()).thenReturn(TURTLE_OUTPUT);
+    }
+
+    @Test
+    public void compileProgram_runsProgramAndReturnsTurtleLines() throws Exception {
+        assertEquals(TURTLE_OUTPUT, logoCompiler.compileProgram(TEST_PROGRAM));
+        verify(program).run(state, turtle);
+    }
+
+    @Test
+    public void compileProgram_whenStoppedWhenRunning_returnsTurtleLines() throws Exception {
+        doThrow(new StopException()).when(program).run(state, turtle);
+        assertEquals(TURTLE_OUTPUT, logoCompiler.compileProgram(TEST_PROGRAM));
+        verify(program).run(state, turtle);
+    }
+
+}
